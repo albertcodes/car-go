e609d723f80d51ced902999e3c3704e1942fdeb4
albertcodes <albertnthiwa@gmail.com>
2021-06-08 06:49:55 +0300

    car-go

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
+import { SlotsComponent } from '../../pages/slots/slots.component';
 import { MapComponent } from '../../pages/map/map.component';
 import { NotificationsComponent } from '../../pages/notifications/notifications.component';
 import { UserComponent } from '../../pages/user/user.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
-import { TypographyComponent } from '../../pages/typography/typography.component';
+import { VehiclesComponent } from '../../pages/vehicles/vehicles.component';
+import { SettingsComponent } from '../../pages/settings/settings.component';
 
 export const AdminLayoutRoutes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'icons', component: IconsComponent },
+  { path: 'slots', component: SlotsComponent },
   { path: 'maps', component: MapComponent },
   { path: 'notifications', component: NotificationsComponent },
   { path: 'user', component: UserComponent },
-  { path: 'tables', component: TablesComponent },
-  { path: 'typography', component: TypographyComponent },
+  { path: 'vehicles', component: VehiclesComponent },
+  { path: 'settings', component: SettingsComponent },
 ];
