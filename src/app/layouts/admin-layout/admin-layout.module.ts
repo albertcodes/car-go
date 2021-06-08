922c1c9df4f73ab3ad37c6976d72a404c38eb0a7
albertcodes <albertnthiwa@gmail.com>
2021-06-08 06:49:50 +0300

    car-go

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,12 +6,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
+import { SlotsComponent } from '../../pages/slots/slots.component';
 import { MapComponent } from '../../pages/map/map.component';
 import { NotificationsComponent } from '../../pages/notifications/notifications.component';
 import { UserComponent } from '../../pages/user/user.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
-import { TypographyComponent } from '../../pages/typography/typography.component';
+import { VehiclesComponent } from '../../pages/vehicles/vehicles.component';
+import { SettingsComponent } from '../../pages/settings/settings.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -26,9 +26,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   declarations: [
     DashboardComponent,
     UserComponent,
-    TablesComponent,
-    IconsComponent,
-    TypographyComponent,
+    VehiclesComponent,
+    SlotsComponent,
+    SettingsComponent,
     NotificationsComponent,
     MapComponent
   ]
