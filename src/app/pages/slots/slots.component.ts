85c53f66564ad459870eaa74197f8c1bf97d8eee
albertcodes <albertnthiwa@gmail.com>
2021-06-08 06:50:26 +0300

    car-go

---
+++ src/app/pages/slots/slots.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slots',
+  templateUrl: 'slots.component.html'
+})
+export class SlotsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
